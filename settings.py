00c25a2705ac9ef5d4e6d605806bbfd5ce45d16d
Donald Casson <donald.casson@gmail.com>
2014-08-22 13:07:56 +1000

    first commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+AUTHOR = 'Donald Casson'
+DISQUS_SITENAME = 'pydonny'
+GITHUB_URL = 'https://github.com/pydonny'
+#GOOGLE_ANALYTICS='UA-18066389-2'
+SITEURL = 'http://pydonny.github.io'
+SITENAME = 'pydonny'
+SOCIAL = (('twitter', 'http://twitter.com/pydonny'),
+                    ('github', 'https://github.com/pydonny'),
+                    ('facebook', 'http://www.facebook.com/donald.casson'),)
+TAG_FEED = 'feeds/%s.atom.xml'
+THEME='notmyidea'
+TWITTER_USERNAME = 'pydonny'
